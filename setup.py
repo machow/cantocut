525f7ede611a7a3ff300152f865adffb881abca3
Michael Chow <machow@princeton.edu>
2018-11-19 15:22:59 -0500

    use correct package structure for resources

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('cantocut.py', 'rb') as f:
+with open('cantocut/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
 setup(
 	name = 'cantocut',
 	version = version,
-	py_modules= ['cantocut'],
+	packages = ['cantocut'],
 	install_requires = ['astar'],
         description = 'word segmentation for cantonese',
         author = 'Michael Chow',
